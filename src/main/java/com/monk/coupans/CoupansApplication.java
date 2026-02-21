369fa45b4b4edc986ec747503847caf2fe13976a
Sweta Kumari <shwetakumari8448@gmail.com>
2026-02-21 17:31:49 +0530

    Intial commit

---
+++ src/main/java/com/monk/coupans/CoupansApplication.java
@@ -0,0 +1,13 @@
+package com.monk.coupans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoupansApplication.class, args);
+	}
+
+}
